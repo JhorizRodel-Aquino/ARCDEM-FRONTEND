82e45ed6dec0b0c42d02e266b4597f6aa6bd1d06
unknown <kinlievenicedeguzman@gmail.com>
2025-04-15 00:55:36 +0800

    admin

--- dist/admin.js
+++ dist/admin.js
@@ -15,3 +15,59 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+// Cursor Coordinates Display
+var coordinates = L.control({ position: "bottomright" });
+
+coordinates.onAdd = function () {
+  var div = L.DomUtil.create("div", "coordinate-display");
+  div.style.padding = "5px";
+  div.style.background = "rgba(255, 255, 255, 0.8)";
+  div.style.borderRadius = "5px";
+  div.innerHTML = "Lat: -, Lng: -";
+  return div;
+};
+
+coordinates.addTo(map);
+
+map.on("mousemove", function (e) {
+  document.querySelector(
+    ".coordinate-display"
+  ).innerHTML = `Lat: ${e.latlng.lat.toFixed(5)}, Lng: ${e.latlng.lng.toFixed(
+    5
+  )}`;
+});
+
+// Base Layers
+let osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+let googleStreets = L.tileLayer(
+  "http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}",
+  {
+    maxZoom: 20,
+    subdomains: ["mt0", "mt1", "mt2", "mt3"],
+  }
+);
+
+googleHybrid = L.tileLayer(
+  "http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}",
+  {
+    maxZoom: 20,
+    subdomains: ["mt0", "mt1", "mt2", "mt3"],
+  }
+);
+
+// Layer Control
+let baseLayers = {
+  OpenStreetMap: osm,
+  "Satellite View": googleHybrid,
+  "Google Street": googleStreets,
+};
+
+// Add Layer Control Button
+L.control.layers(baseLayers, null, { position: "bottomright" }).addTo(map);
+
+L.control.zoom({ position: "bottomright" }).addTo(map);
